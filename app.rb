2d62c2877153500bf3d6f442cde96c08811808a2
Nikki Thean <nikki.thean@gmail.com>
2013-12-09 12:47:53 -0500

    Initial commit

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
-require 'sinatra/base'
+require 'bundler'
+Bundler.require
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
     get '/' do
       "hello world!"
+    end
+
+    get '/hello-world' do
+      @random_numbers = Array.new(20) {|i| 1 + i}
+      erb :hello
+    end
+
+    get '/artists' do
+      @artists = ["Neko Case", "Lissie", "Ed Sheeran", "Taylor Hanson"]
+      erb :artists
     end
   end
 end
